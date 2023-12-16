a48bf4005311ba90d2866bbe2f6a883b40b75f50
Yusuf-R <y.abdulwasiu@gmail.com>
2023-12-16 23:18:18 +0100

    feat: :sparkles: code refactor using Mongoose to connect to our DB

--- src/test/db.test.js
+++ src/test/db.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable func-names */
 /* eslint-disable jest/prefer-lowercase-title */
 /* eslint-disable jest/require-hook */
@@ -6,9 +7,20 @@
 const { describe, it, before, after } = require('mocha');
 const { expect } = require('chai');
 const sinon = require('sinon');
+const mongoose = require('mongoose');
 const dbClient = require('../utils/db');
 
 describe('DBClient', () => {
+  // Stub the mongoose connect method to resolve immediately
+  before(() => {
+    sinon.stub(mongoose, 'connect').resolves();
+  });
+
+  // Restore the original mongoose connect method
+  after(() => {
+    sinon.restore();
+  });
+
   // Initialize the database client before running the tests
   before(async function () {
     // Increase the timeout for the before hook
@@ -20,8 +32,10 @@ describe('DBClient', () => {
   });
 
   after(async () => {
-    // Close the MongoDB connection after all tests
-    await dbClient.client.close();
+    // If the client is defined, close the MongoDB connection after all tests
+    if (dbClient.client) {
+      await dbClient.client.close();
+    }
   });
 
   it('should be alive', async () => {
@@ -30,12 +44,20 @@ describe('DBClient', () => {
   });
 
   it('should handle connection errors', async () => {
+    // Stub the isAlive function to resolve immediately
+    sinon.stub(dbClient, 'isAlive').resolves(false);
+
     // Close the MongoDB connection to simulate an error
-    if (dbClient && dbClient.client) {
+    if (dbClient.client) {
       await dbClient.client.close();
     }
+
     const isAlive = await dbClient.isAlive();
     expect(isAlive).to.equal(false);
+
+    // Restore the original isAlive function
+    sinon.restore();
   });
+
   // Add more tests as needed
 });
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,46 +1,39 @@
-/* eslint-disable max-len */
-const password = 'schoolpilot';
-const { MongoClient } = require('mongodb');
+/* eslint-disable jest/require-hook */
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
 
+const password = 'schoolpilot';
 const uri = `mongodb+srv://alxSE:${password}@schoolpilot.0zpvosm.mongodb.net/?retryWrites=true&w=majority`;
 
 class DBClient {
   constructor() {
-    // Create a MongoClient with the connection URI
-    this.client = new MongoClient(uri, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    // initialize the client with the connection to the database
     this.init();
   }
 
   async init() {
     try {
-      // Connect the client to the server (optional starting in v4.7)
-      await this.client.connect();
-      console.log('Connected to MongoDB!');
+      this.client = await mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      // Access the Mongoose connection object only after it's returned by connect
+      console.log('Connected to MongoDB established successfully!');
     } catch (error) {
       console.error('Error connecting to MongoDB:', error);
     }
   }
 
-  // a function isAlive that returns true when the connection to MongoDB is a success otherwise, false
   async isAlive() {
+    // Check if the Mongoose connection state is open
     try {
-      // Check if MongoDB is connected
-      if (await this.client.isConnected()) {
-        // Send a ping to confirm a successful connection
-        await this.client.db('admin').command({ ping: 1 });
-        return true;
-      }
-      return false;
+      await this.client;
+      return true;
     } catch (error) {
-      console.error('Error pinging MongoDB:', error);
       return false;
     }
   }
 }
 
-// Export the instance directly
-module.exports = new DBClient();
+const dbClient = new DBClient();
+module.exports = dbClient;
