d845da75485199ffa768f56a1ee6133cd0fbca0a
Donald Ajaps <ajapon@yahoo.com>
2023-12-24 23:46:46 +0100

    Update course model's database index
    * Change index for course collection to enforce course code uniqueness only for courses in the same department.

--- src/models/course.js
+++ src/models/course.js
@@ -6,13 +6,16 @@ const { ObjectId, enums } = require('./base');
 // Course properties
 const course = {
   department: { type: ObjectId, ref: 'Department', required: true },
-  courseCode: { type: String, unique: true, required: true },
+  courseCode: { type: String, required: true },
   name: { type: String, required: true },
   level: { type: Number, enum: enums.courses.levels, required: true },
   semester: { type: Number, enum: enums.courses.semesters, required: true },
   units: { type: Number, required: true },
 };
 const courseSchema = new mongoose.Schema({ ...course }, { timestamps: true });
+
+// Prevent duplicate course codes in same department
+courseSchema.index({ department: 1, courseCode: 1 }, { unique: true });
 
 // Course class
 const Course = mongoose.model('Course', courseSchema);
